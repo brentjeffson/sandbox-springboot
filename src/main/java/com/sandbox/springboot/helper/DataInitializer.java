3b1cacf1ba75e30440fd53787fee4eeb997fcac9
Brent Jeffson Florendo <brentjeffson@gmail.com>
2025-02-21 07:26:40 +0800

    Apply best practices to a command line runner data initializer

--- src/main/java/com/sandbox/springboot/helper/DataInitializer.java
+++ src/main/java/com/sandbox/springboot/helper/DataInitializer.java
@@ -1,5 +1,7 @@
 package com.sandbox.springboot.helper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
@@ -8,35 +10,54 @@ import org.springframework.stereotype.Component;
 
 import com.sandbox.springboot.model.User;
 import com.sandbox.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 
 @Component
 @DependsOnDatabaseInitialization
 public class DataInitializer implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(DataInitializer.class);
 
     @Autowired
     private UserRepository userRepository;
     
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Value("${initial-users.admin.username}")
+    private String adminUsername;
+
+    @Value("${initial-users.admin.password}")
+    private String adminPassword;
+
+    @Value("${initial-users.user.username}")
+    private String userUsername;
+
+    @Value("${initial-users.user.password}")
+    private String userPassword;
     
     @Override
     public void run(String... args) {
-        // Create admin user if it doesn't exist
-        if (userRepository.findByUsername("admin").isEmpty()) {
-            User admin = new User();
-            admin.setUsername("admin");
-            admin.setPassword(passwordEncoder.encode("admin"));
-            admin.setRole("ROLE_ADMIN");
-            userRepository.save(admin);
-        }
-        
-        // Create regular user if it doesn't exist
-        if (userRepository.findByUsername("user").isEmpty()) {
-            User user = new User();
-            user.setUsername("user");
-            user.setPassword(passwordEncoder.encode("password"));
-            user.setRole("ROLE_USER");
-            userRepository.save(user);
+        String[][] users = {
+            {adminUsername, adminPassword, "ROLE_ADMIN"},
+            {userUsername, userPassword, "ROLE_USER"}
+        };
+
+        for (String[] userData : users) {
+            String username = userData[0];
+            String password = userData[1];
+            String role = userData[2];
+
+            if (userRepository.findByUsername(username).isEmpty()) {
+                User user = new User();
+                user.setUsername(username);
+                user.setPassword(passwordEncoder.encode(password));
+                user.setRole(role);
+                userRepository.save(user);
+                logger.info("Created user: {} with role: {}", username, role);
+            } else {
+                logger.info("User: {} already exists", username);
+            }
         }
     }
 }
