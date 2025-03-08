7dcac52d0205608d3607f503d8f108ce8a6311eb
Brent Jeffson Florendo <brentjeffson@gmail.com>
2025-03-08 20:10:58 +0800

    Running with commandline runner

--- src/main/java/com/sandbox/springboot/SpringbootApplication.java
+++ src/main/java/com/sandbox/springboot/SpringbootApplication.java
@@ -1,13 +1,19 @@
 package com.sandbox.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello, world!");
 	}
 
 }
